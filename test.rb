2f843e16b931c12f5a0f4dc9e5a6f1afbbd2e400
yuya yamamoto <yuya.yamamoto2000@gmail.com>
2020-07-30 16:14:20 +0900

    WEB技術入門シリーズ課題訂正版

--- test.rb
+++ test.rb
@@ -7,7 +7,7 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/test', WEBrick::HTTPServlet::ERBHandler, '.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'kadai.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.start
